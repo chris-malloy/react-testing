e4f3450acedb1b913e9cd02cf8c42ff54d6fed18
chris-malloy <chrismalloymusic@gmail.com>
2018-03-04 13:06:21 -0500

    refactor commentBox test

--- test/components/commentBoxTest.js
+++ test/components/commentBoxTest.js
@@ -3,18 +3,21 @@ import CommentBox from '../../src/components/CommentBox';
 
 
 describe('Comment Box', () => {
+    let component; // undefined
+
+    beforeEach(() => { // gets called for each it
+        component = renderComponent(CommentBox);
+    })
+
     it('has the correct class', () => {
-        const component = renderComponent(CommentBox);
         expect(component).to.have.class('comment-box');
     });
 
     it('has a text area', () => {
-        const component = renderComponent(CommentBox);
         expect(component.find('textarea')).to.exist;
     });
 
     it('has a button', () => {
-        const component = renderComponent(CommentBox);
         expect(component.find('button')).to.exist;
     });
 
